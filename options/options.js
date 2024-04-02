40db911e5147a90cd083c905cd920b842d32546e
Crowdbotics <team@crowdbotics.com>
2024-04-02 14:23:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "whiskey-cave-47357",
+  url: "https://whiskey-cave-47357.botics.co",
+  api: "https://whiskey-cave-47357.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
